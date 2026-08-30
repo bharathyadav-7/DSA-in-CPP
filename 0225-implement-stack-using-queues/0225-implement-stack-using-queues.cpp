5d0cd225bc2e48abbb88fa8b6c5bdb1de36bffcd
Katuru Bharath <bharathyadav.katuru07@gmail.com>
2026-08-30 11:09:31 +0530

    Time: 0 ms (100%), Space: 10 MB (59.98%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -0,0 +1,36 @@
+class MyStack {
+public:
+    queue<int> q;
+
+    MyStack() {
+    }
+
+    void push(int x) {
+        q.push(x);
+
+        int n = q.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop() {
+        int x = q.front();
+        q.pop();
+        return x;
+    }
+
+    int top() {
+        return q.front();
+    }
+
+    bool empty() {
+        return q.empty();
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
