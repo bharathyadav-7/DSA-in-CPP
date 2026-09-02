7cadcc41088a1ea201cbd1fd79ed78a1be523af6
Katuru Bharath <bharathyadav.katuru07@gmail.com>
2026-09-02 18:00:54 +0530

    Time: 0 ms (100%), Space: 10.3 MB (84.83%) - LeetHub - Time: O(N * 2^N), Space: O(N * 2^N)

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+void find(int ind,vector<int> &nums,vector<int> &ds,vector<vector<int>>&ans){
+    ans.push_back(ds);
+    for(int i=ind;i<nums.size();i++){
+        if(i!=ind && nums[i]==nums[i-1])continue;
+        ds.push_back(nums[i]);
+        find(i+1,nums,ds,ans);
+        ds.pop_back();
+    }
+}
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int> ds;
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        find(0,nums,ds,ans);
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
